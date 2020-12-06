41b93b61233ef5ddab12ebadb295348ebb1879ab
Dan Walker <dan.lawrence.walker@gmail.com>
2020-12-06 13:59:29 -0500

    - completed test and class files for list implementation of stack

--- src/main/java/ListStack.java
+++ src/main/java/ListStack.java
@@ -1,6 +1,10 @@
+import java.util.EmptyStackException;
+
 public class ListStack<T> {
 
     private ListNode top;
+    // testing purposes
+    private int size = 0;
 
     private class ListNode<T> {
         private T data;
@@ -21,10 +25,46 @@ public class ListStack<T> {
         this.top = new ListNode();
     }
 
-    public void push(T data) {
-        if (top.next == null) {
-            top.next = new ListNode(data);
+    public <T> Object pop() throws EmptyStackException {
+        if(top.next == null) {
+            throw new EmptyStackException();
         }
+
+        // unlink
+        ListNode temp = top.next;
+        top.next = temp.next;
+        size--;
+
+        return temp.data;
+    }
+
+    public <T> Object peek() throws EmptyStackException {
+        if(top.next == null) {
+            throw new EmptyStackException();
+        }
+        return top.next.data;
+    }
+
+    public void push(T data) {
+        ListNode newNode = new ListNode(data);
+
+        // empty stack
+        if (top.next == null) {
+            top.next = newNode;
+        } else {
+            // link nodes
+            ListNode temp = top.next;
+            top.next = newNode;
+            newNode.next = temp;
+        }
+
+        // testing purposes
+        size++;
+    }
+
+    // testing purposes
+    public int getSize() {
+        return size;
     }
 
     public String toString() {
@@ -35,9 +75,10 @@ public class ListStack<T> {
         ListNode temp = top.next;
 
         template += "top: ";
-        while(temp.next != null) {
+        while(temp != null) {
             template += temp.data;
             template += "\nv\n";
+            temp = temp.next;
         }
 
         template += "null";
--- src/test/java/ListStackTest.java
+++ src/test/java/ListStackTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.*;
 
+import java.util.EmptyStackException;
+
 public class ListStackTest {
 
     ListStack test_stack;
@@ -12,7 +14,8 @@ public class ListStackTest {
     @Test
     @DisplayName("after calling the ListStack constructor ")
     void testStackConstructor() {
-        Assertions.assertNotNull(test_stack, "test_stack should not be initialized");
+        Assertions.assertNotNull(test_stack,
+                "test_stack should not be initialized");
     }
 
 
@@ -34,11 +37,170 @@ public class ListStackTest {
             Assertions.assertEquals("top: " + 0 + "\nv\n" + "null", test_stack.toString());
         }
 
-        @Disabled
         @Test
         @DisplayName("displaying a stack with multiple members ")
         void testMultiElementStackString() {
+            for(int i = 0; i < 5; i++) {
+                test_stack.push(i);
+            }
+                Assertions.assertEquals("top: " + 4 + "\nv\n" + 3 + "\nv\n"
+                        + 2 + "\nv\n" + 1 + "\nv\n" + 0 + "\nv\n" + "null", test_stack.toString());
+        }
+    }
 
+    @Nested
+    @DisplayName("push method tests")
+    class testPush {
+
+        @Test
+        @DisplayName("pushing a single integer ")
+        void testPushInt() {
+            test_stack.push(0);
+            Assertions.assertEquals(1, test_stack.getSize(),
+                    "size should now be 1");
+        }
+
+        @Test
+        @DisplayName("pushing a single String ")
+        void testPushString() {
+            test_stack.push("hello");
+            Assertions.assertEquals(1, test_stack.getSize(),
+                    "size should now be 1");
+        }
+
+        @Test
+        @DisplayName("pushing (10) elements of same type ")
+        void testPushMultiSingleType() {
+            for(int i = 0; i < 10; i++) {
+                test_stack.push(i);
+            }
+            Assertions.assertEquals(10, test_stack.getSize(),
+                    "size should be (10)");
+        }
+
+        @Test
+        @DisplayName("pushing (10) elements of varying type ")
+        void testPushMultiManyType() {
+            for(int i = 0; i < 10; i++) {
+                if(i % 2 == 0) {
+                    test_stack.push(Double.valueOf(i));
+                } else if(i % 7 == 0){
+                    test_stack.push(i);
+                } else {
+                    test_stack.push(String.valueOf(i));
+                }
+            }
+            Assertions.assertEquals(10, test_stack.getSize(),
+                    "size should equal 10 with no failures");
+        }
+
+        @Test
+        @DisplayName("pushing 10,000 elements ")
+        void testPushLargeStack() {
+            for(int i = 0; i < 10000; i++) {
+                test_stack.push(i);
+            }
+            Assertions.assertEquals(10000, test_stack.getSize(),
+                    "size should equal 10,000");
+        }
+    }
+
+    @Nested
+    @DisplayName("peek method tests")
+    class testPeek {
+
+        @Test
+        @DisplayName("peeking at empty stack ")
+        void testPeakEmptyStack() {
+            Assertions.assertThrows(EmptyStackException.class, () -> {test_stack.peek();},
+                    "should throw an EmptyStackException");
+        }
+
+        @Test
+        @DisplayName("peeking after single push ")
+        void testPeekSinglePush() {
+            test_stack.push("hello");
+            Assertions.assertAll(
+                    () -> Assertions.assertEquals("hello", test_stack.peek(),
+                        "should return \"hello\""),
+                    () -> Assertions.assertEquals(1, test_stack.getSize(),
+                            " and should not delete the element")
+            );
+        }
+
+        @Test
+        @DisplayName("peeking after multiple pushes ")
+        void testPeekMultiPush() {
+            for(int i = 0; i < 10; i++) {
+                test_stack.push(i);
+            }
+            Assertions.assertAll(
+                    () -> Assertions.assertEquals(9, test_stack.peek(),
+                            "should return the last value pushed (9)"),
+                    () -> Assertions.assertEquals(10, test_stack.getSize(),
+                            " and should not delete the element")
+            );
+        }
+    }
+
+    @Nested
+    @DisplayName("pop method tests")
+    class testPop {
+
+        @Test
+        @DisplayName("popping from empty stack ")
+        void testPopEmptyStack() {
+            Assertions.assertThrows(EmptyStackException.class, () -> {test_stack.pop();},
+                    " should throw EmptyStackException");
+        }
+
+        @Test
+        @DisplayName("single pop after single push ")
+        void testSinglePopSinglePush() {
+            test_stack.push("hello");
+            Assertions.assertAll(
+                    () -> Assertions.assertEquals("hello", test_stack.pop(),
+                            "should return \"hello\""),
+                    () -> Assertions.assertEquals(0, test_stack.getSize(),
+                            " and delete the element from the stack.")
+            );
+        }
+
+        @Test
+        @DisplayName("multiple pops after single push ")
+        void testMultiPopSinglePush() {
+            test_stack.push("hello");
+            test_stack.pop();
+            Assertions.assertThrows(EmptyStackException.class, () -> {test_stack.pop();},
+                    " should throw EmptyStackException");
+        }
+
+        @Test
+        @DisplayName("single pop after multiple pushes")
+        void testSinglePopMultiPush() {
+            for(int i = 0; i < 10; i++) {
+                test_stack.push(i);
+            }
+            Assertions.assertAll(
+                    () -> Assertions.assertEquals(9, test_stack.pop(),
+                            " should return last element pushed (9)"),
+                    () -> Assertions.assertEquals(9, test_stack.getSize(),
+                            " and delete it from the stack.")
+            );
+        }
+
+        @Test
+        @DisplayName("multiple pops after multiple pushes")
+        void testMultiPopMultiPush() {
+            for(int i = 0; i < 10; i++) {
+                test_stack.push(i);
+            }
+            for(int i = 9; i >= 0; i--) {
+                Assertions.assertEquals(i, test_stack.pop(),
+                        " should pop in reverse order of push");
+            }
+            Assertions.assertEquals(0, test_stack.getSize(),
+                    " and result in an empty stack");
         }
     }
 }
