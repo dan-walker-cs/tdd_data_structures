40f909ef6e50fd345861d4d5276b7a1a4128fb11
Dan Walker <dan.lawrence.walker@gmail.com>
2020-12-09 17:42:31 -0500

    - added implementation and test class for MaxHeap

--- src/main/java/heaps/MaxHeap.java
+++ src/main/java/heaps/MaxHeap.java
@@ -1,4 +1,115 @@
 package heaps;
 
+import java.util.NoSuchElementException;
+
 public class MaxHeap {
+
+    int[] heap;
+    int currentSize;
+    int maxSize;
+
+    public MaxHeap() {
+        this.maxSize = 5;
+        this.currentSize = 0;
+        this.heap = new int[maxSize];
+    }
+
+    MaxHeap(int maxSize) {
+        this.maxSize = maxSize;
+        this.currentSize = 0;
+        this.heap = new int[maxSize];
+    }
+
+    public int remove() throws NoSuchElementException {
+        if(isEmpty()) {
+            throw new NoSuchElementException();
+        }
+
+        int root = heap[0];
+        heap[0] = heap[--currentSize];
+        heap[currentSize] = 0;
+
+        maxHeapify(0);
+
+        return root;
+    }
+
+    private void maxHeapify(int currentIndex) {
+        if(!isLeaf(currentIndex)) {
+            int leftChild = leftChildIndex(currentIndex);
+            int rightChild = rightChildIndex(currentIndex);
+            if(heap[leftChild] > heap[currentIndex] || heap[rightChild] > heap[currentIndex]) {
+                if(heap[leftChild] > heap[rightChild]) {
+                    swap(leftChild, currentIndex);
+                    currentIndex = leftChild;
+                } else {
+                    swap(rightChild, currentIndex);
+                    currentIndex = rightChild;
+                }
+                maxHeapify(currentIndex);
+            }
+        }
+    }
+
+    public void insert(int data) throws NoSuchElementException {
+        if(isFull()) {
+            throw new NoSuchElementException();
+        }
+        heap[currentSize] = data;
+        int currentIndex = currentSize;
+        currentSize++;
+
+        while(heap[currentIndex] > heap[parentIndex(currentIndex)]) {
+            swap(currentIndex, parentIndex(currentIndex));
+            currentIndex = parentIndex(currentIndex);
+        }
+    }
+
+    private void swap(int index1, int index2) {
+        int temp = heap[index1];
+        heap[index1] = heap[index2];
+        heap[index2] = temp;
+    }
+
+    private boolean isEmpty() {
+        if(currentSize == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isFull() {
+        if(currentSize == maxSize) {
+            return true;
+        }
+        return false;
+    }
+
+    private int parentIndex(int currentIndex) {
+        return (currentIndex - 1) / 2;
+    }
+
+    private int leftChildIndex(int currentIndex) {
+        return currentIndex * 2 + 1;
+    }
+
+    private int rightChildIndex(int currentIndex) {
+        return currentIndex * 2 + 2;
+    }
+
+    private boolean isLeaf(int currentIndex) {
+        if(currentIndex > currentSize/2 && currentIndex < currentSize) {
+            return true;
+        }
+        return false;
+    }
+
+    public String toString() {
+        String template = "[";
+        for(int i = 0; i < maxSize-1; i++) {
+            template += heap[i] + ", ";
+        }
+        template += heap[maxSize-1] + "]";
+        return template;
+    }
 }
--- src/test/java/heapTests/MaxHeapTest.java
+++ src/test/java/heapTests/MaxHeapTest.java
@@ -1,4 +1,108 @@
 package heapTests;
 
+import heaps.MaxHeap;
+import org.junit.jupiter.api.*;
+
+import java.util.NoSuchElementException;
+
 public class MaxHeapTest {
+
+    MaxHeap test_heap;
+
+    @BeforeEach
+    void init() {
+        test_heap = new MaxHeap();
+    }
+
+    @Nested
+    @DisplayName("tests for insert method")
+    class testInsert {
+
+        @Test
+        @DisplayName("single insert")
+        void testSingleInsert() {
+            test_heap.insert(1);
+            Assertions.assertEquals("[1, 0, 0, 0, 0]", test_heap.toString(),
+                    " should result in that element being stored at the root.");
+        }
+
+        @Test
+        @DisplayName("multi ordered insert")
+        void testMultiOrderedInsert() {
+            for(int i = 0; i < 5; i++) {
+                test_heap.insert(i * 4);
+            }
+            Assertions.assertEquals("[16, 12, 4, 0, 8]", test_heap.toString(),
+                    " should result in elements being added and heap structure being preserved.");
+        }
+
+        @Test
+        @DisplayName("multi jumbled insert")
+        void testMultiUnorderedInsert() {
+            test_heap.insert(0);
+            test_heap.insert(20);
+            test_heap.insert(5);
+            test_heap.insert(8);
+            test_heap.insert(50);
+
+            Assertions.assertEquals("[50, 20, 5, 0, 8]", test_heap.toString(),
+                    " should result in elements being added and heap structure being preserved.");
+        }
+
+        @Test
+        @DisplayName("insert on full heap")
+        void testInsertHeapOverflow() {
+            for(int i = 0; i < 5; i++) {
+                test_heap.insert(i);
+            }
+            Assertions.assertThrows(NoSuchElementException.class, () -> test_heap.insert(5),
+                    " should throw NoSuchElementException.");
+        }
+    }
+
+    @Nested
+    @DisplayName("tests for remove method")
+    class testRemove {
+
+        @Test
+        @DisplayName("single remove from populated heap")
+        void testSingleRemove() {
+            for(int i = 1; i < 6; i++) {
+                test_heap.insert(i);
+            }
+            int test_remove = test_heap.remove();
+            Assertions.assertAll(
+                    () -> Assertions.assertEquals(5, test_remove,
+                            " should return the root element"),
+                    () -> Assertions.assertEquals("[4, 3, 2, 1, 0]", test_heap.toString(),
+                            " and preserve heap properties.")
+            );
+        }
+
+        @Test
+        @DisplayName("remove from empty heap")
+        void testRemoveEmptyHeap() {
+            Assertions.assertThrows(NoSuchElementException.class, () -> test_heap.remove(),
+                    " should throw NoSuchElementException");
+        }
+
+        @Test
+        @DisplayName("multi-remove from populated heap")
+        void testMultiRemove() {
+            for(int i = 1; i < 6; i++) {
+                test_heap.insert(i);
+            }
+            int[] test_remove = new int[5];
+            int[] expected = {5, 4, 3, 2, 1};
+            for(int i = 0; i < 5; i++) {
+                test_remove[i] = test_heap.remove();
+            }
+            Assertions.assertAll(
+                    () -> Assertions.assertArrayEquals(expected, test_remove,
+                            " should result in elements received in descending order"),
+                    () -> Assertions.assertEquals("[0, 0, 0, 0, 0]", test_heap.toString(),
+                            " and the heap becoming empty")
+            );
+        }
+    }
 }
