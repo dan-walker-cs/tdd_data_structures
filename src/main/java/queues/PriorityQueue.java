3b0644d37f8b2812f51422590d723c0a85f43f92
Dan Walker <dan.lawrence.walker@gmail.com>
2020-12-10 12:31:10 -0500

    - added implementation and test class for Generic Type priority queue

--- src/main/java/queues/PriorityQueue.java
+++ src/main/java/queues/PriorityQueue.java
@@ -1,4 +1,151 @@
 package queues;
 
-public class PriorityQueue {
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+public class PriorityQueue<T> {
+
+    PQnode[] p_queue;
+    int currentSize;
+    int maxSize;
+
+    private class PQnode<T> {
+        T data;
+        int priority;
+        static final int MIN_PRIORITY = 1;
+
+        PQnode(T data) {
+            this.data = data;
+            this.priority = MIN_PRIORITY;
+        }
+
+        PQnode(T data, int priority) {
+            this.data = data;
+            this.priority = priority;
+        }
+
+        public String toString() {
+            String template = "(" + data.toString() + "," + priority + ")";
+            return template;
+        }
+    }
+
+    public PriorityQueue() {
+        this.maxSize = 5;
+        this.p_queue = new PQnode[maxSize];
+        this.currentSize = 0;
+    }
+
+    PriorityQueue(int maxSize) {
+        this.maxSize = maxSize;
+        this.p_queue = new PQnode[maxSize];
+        this.currentSize = 0;
+    }
+
+    public T remove() throws NoSuchElementException {
+        if(isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        PQnode root = p_queue[0];
+        p_queue[0] = p_queue[--currentSize];
+        p_queue[currentSize] = null;
+
+        maxHeapify(0);
+
+        return (T)root.data;
+    }
+
+    private void maxHeapify(int currentIndex) {
+        if(!isLeaf(currentIndex) && p_queue[currentIndex] != null) {
+            int leftPriority = 0;
+            int rightPriority = 0;
+            if(p_queue[leftChildIndex(currentIndex)] == null) {
+                leftPriority = Integer.MIN_VALUE;
+            } else if(p_queue[rightChildIndex(currentIndex)] == null) {
+                rightPriority = Integer.MIN_VALUE;
+            } else {
+                leftPriority = p_queue[leftChildIndex(currentIndex)].priority;
+                rightPriority = p_queue[rightChildIndex(currentIndex)].priority;
+            }
+
+            if(leftPriority > p_queue[currentIndex].priority
+                    || rightPriority > p_queue[currentIndex].priority) {
+                if(leftPriority > rightPriority) {
+                    swap(leftChildIndex(currentIndex), currentIndex);
+                    currentIndex = leftChildIndex(currentIndex);
+                } else {
+                    swap(rightChildIndex(currentIndex), currentIndex);
+                    currentIndex = rightChildIndex(currentIndex);
+                }
+                maxHeapify(currentIndex);
+            }
+        }
+    }
+
+    public void insert(T data) {
+        insert(data, PQnode.MIN_PRIORITY);
+    }
+
+    public void insert(T data, int priority) throws NoSuchElementException {
+        if(isFull()) {
+            throw new NoSuchElementException();
+        }
+        int currentIndex = currentSize;
+        PQnode newNode = new PQnode(data, priority);
+        p_queue[currentIndex] = newNode;
+        currentSize++;
+
+        while(p_queue[currentIndex].priority > p_queue[parentIndex(currentIndex)].priority) {
+            swap(currentIndex, parentIndex(currentIndex));
+            currentIndex = parentIndex(currentIndex);
+        }
+    }
+
+    private void swap(int index1, int index2) {
+        PQnode temp = p_queue[index1];
+        p_queue[index1] = p_queue[index2];
+        p_queue[index2] = temp;
+    }
+
+    private boolean isEmpty() {
+        if(currentSize == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isFull() {
+        if(currentSize == maxSize) {
+            return true;
+        }
+        return false;
+    }
+
+    private int parentIndex(int currentIndex) {
+        return (currentIndex - 1) / 2;
+    }
+
+    private int leftChildIndex(int currentIndex) {
+        return currentIndex * 2 + 1;
+    }
+
+    private int rightChildIndex(int currentIndex) {
+        return currentIndex * 2 + 2;
+    }
+
+    private boolean isLeaf(int currentIndex) {
+        if(currentIndex > (currentSize / 2) && currentIndex < currentSize) {
+            return true;
+        }
+        return false;
+    }
+
+    public String toString() {
+        String template = "[";
+        for(int i = 0; i < maxSize-1; i++) {
+            template += p_queue[i] + ", ";
+        }
+        template += p_queue[maxSize-1] + "]";
+        return template;
+    }
 }
--- src/test/java/queueTests/PriorityQueueTest.java
+++ src/test/java/queueTests/PriorityQueueTest.java
@@ -1,4 +1,113 @@
 package queueTests;
 
-public class PriorityQueueTest {
+import org.junit.jupiter.api.*;
+import queues.PriorityQueue;
+
+import java.util.NoSuchElementException;
+
+public class PriorityQueueTest<T> {
+
+    PriorityQueue test_pq;
+
+    @BeforeEach
+    void init() {
+        test_pq = new PriorityQueue();
+    }
+
+    @Nested
+    @DisplayName("tests for insert method")
+    class testInsert {
+
+        @Test
+        @DisplayName("generics test: inserting int")
+        void testSingleInsertInt() {
+            test_pq.insert(5);
+            Assertions.assertEquals("[(5,1), null, null, null, null]", test_pq.toString(),
+                    " should store a pair (data, priority) at the root node.");
+        }
+
+        @Test
+        @DisplayName("generics test: inserting String")
+        void testSingleInsertString() {
+            test_pq.insert("hello");
+            Assertions.assertEquals("[(hello,1), null, null, null, null]", test_pq.toString(),
+                    " should store a pair (data, priority) at the root node.");
+        }
+
+        @Test
+        @DisplayName("insert to fill queue, all same priority")
+        void testMultiInsertSamePriority() {
+            for(int i = 1; i < 6; i++) {
+                test_pq.insert(i);
+            }
+            Assertions.assertEquals("[(1,1), (2,1), (3,1), (4,1), (5,1)]", test_pq.toString(),
+                    " should store pairs in same order they are inserted.");
+        }
+
+        @Test
+        @DisplayName("insert to fill queue, varying priority")
+        void testMultiInsertMultiPriority() {
+            for(int i = 1; i < 6; i++) {
+                test_pq.insert(i/2+1, i);
+            }
+            Assertions.assertEquals("[(3,5), (3,4), (2,2), (1,1), (2,3)]", test_pq.toString(),
+                    " should store pairs such that priority is maximized in heap structure.");
+        }
+
+        @Test
+        @DisplayName("insert into full priority queue")
+        void testInsertFullQueue() {
+            for(int i = 0; i < 5; i++) {
+                test_pq.insert(i);
+            }
+            Assertions.assertThrows(NoSuchElementException.class, () -> test_pq.insert(5),
+                    " should throw NoSuchElementException.");
+        }
+    }
+
+    @Nested
+    @DisplayName("tests for remove method")
+    class testRemove {
+
+        @Test
+        @DisplayName("removal from empty queue")
+        void testRemoveEmptyQueue() {
+            Assertions.assertThrows(NoSuchElementException.class, () -> test_pq.remove(),
+                " should throw NoSuchElementException.");
+        }
+
+        @Test
+        @DisplayName("removal from single element queue")
+        void testSingleRemoveSingleElementQueue() {
+            test_pq.insert("hello");
+            String data = test_pq.remove().toString();
+
+            Assertions.assertAll(
+                    () -> Assertions.assertEquals("hello", data,
+                            " should return the root element"),
+                    () -> Assertions.assertEquals("[null, null, null, null, null]", test_pq.toString(),
+                            " and result in an empty queue.")
+            );
+        }
+
+        @Test
+        @DisplayName("emptying a full queue")
+        void testFullRemovalQueue() {
+            for(int i = 1; i < 6; i++) {
+                test_pq.insert(i, i);
+            }
+            String[] data = new String[5];
+            String[] expected = {"5", "4", "3", "2", "1"};
+
+            for(int i = 0; i < 5; i++) {
+                data[i] = test_pq.remove().toString();
+            }
+            Assertions.assertAll(
+                    () -> Assertions.assertArrayEquals(expected, data,
+                            " should return the elements in order of decreasing priority"),
+                    () -> Assertions.assertEquals("[null, null, null, null, null]", test_pq.toString(),
+                            " and result in an empty queue.")
+            );
+        }
+    }
 }
